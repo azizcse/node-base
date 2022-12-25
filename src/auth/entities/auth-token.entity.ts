51b6c259f40fba32b3e72f10373592f1a10c9641
Aziz <imazizul@gmail.com>
2022-12-25 09:50:49 +0600

    Auth guardx

---
+++ src/auth/entities/auth-token.entity.ts
@@ -0,0 +1,16 @@
+import { sign } from 'jsonwebtoken';
+
+export class RefreshToken{
+    constructor(init?:Partial<RefreshToken>){
+        Object.assign(this,init);
+    }
+
+    id:number;
+    userId:number;
+    userAgent:string;
+    ipAddress:string;
+
+    sign():string{
+        return sign({...this}, process.env.REFRESH_SECRET);
+    }
+}
